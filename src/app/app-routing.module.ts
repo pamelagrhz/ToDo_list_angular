0a42d2a975112d5d91f9c4eee2f479e9459cde1d
pamelagrhz <pamelagrhz@gmail.com>
2019-04-18 15:00:05 -0500

    botones con color de login y signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from 'src/app/login/login.component';
+import { SignupComponent } from 'src/app/signup/signup.component';
 
 const routes: Routes = [{
   path: 'login',
   component: LoginComponent
-
+},{
+  path: 'signup',
+  component:SignupComponent
 }];
 
 @NgModule({
